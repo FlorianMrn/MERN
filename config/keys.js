88d33b8831a34b80f68934e28aaea37aa02d999b
Florian Merrien <fmerrien.dev@gmail.com>
2020-05-26 18:17:15 +0200

    Passport config file added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb+srv://admin:glBcwrEqItx2bsAO@cluster0-o5plw.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI : 'mongodb+srv://admin:glBcwrEqItx2bsAO@cluster0-o5plw.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: "secret"
 };
--- routes/API/users.js
+++ routes/API/users.js
@@ -11,3 +11,37 @@ const validateLoginInput = require('../../validation/login');
 
 // Load User Model
 const User = require('../../models/User');
+
+router.post("/register", (req, res) => {
+
+    // Form validation
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ email : req.body.email}).then(user => {
+        if(user) {
+            return res.status(400).json({ email : "Email already exists"});
+        } else {
+            const newUser = new User({
+                name : req.body.name,
+                email : req.body.email,
+                password : req.body.password
+            });
+
+            // Hash Password
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+                });
+            });
+        };
+    });
+});
