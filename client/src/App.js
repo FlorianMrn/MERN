19a2822e98201521114a2636e978e06c144208e5
Florian Merrien <fmerrien.dev@gmail.com>
2020-05-27 11:44:35 +0200

    Landing, Nav, Register nad login components done, next step : Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
+// Import
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
     </div>
   );
 }
 
+// Export
 export default App;
